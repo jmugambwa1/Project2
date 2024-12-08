c974ac4ffe4162756fe05d5486a678c17a08081e
Joel Mugambwa <khezajoel@gmail.com>
2024-12-08 15:52:59 +0200

    Added homepage with Bootstrap layout

---
+++ src/resources/views/home/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+    <h1>Welcome to Project 2!</h1>
+    <p>This is your homepage.</p>
+    <a class="btn btn-primary" href="#">Learn More</a>
+@endsection
+
